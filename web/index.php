df36deb1ac6c8b615411cf36e26e1bedaf9fcdce
Timothy Coleman <timhc22@gmail.com>
2015-09-06 13:09:42 +0100

    post

--- web/index.php
+++ web/index.php
@@ -1,5 +1,7 @@
 <?php
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
@@ -7,7 +9,7 @@ $app = new Silex\Application();
 $app['debug'] = true; // todo set to false in production
 
 $items = [
-	'1'=> [
+	'1' => [
 		'name' => 'item1',
 	],
 	'2' => [
@@ -15,15 +17,40 @@ $items = [
 	],
 ];
 
+/**
+ * GET
+ */
 $app->get('/', function() use ($items) {
 	return json_encode($items);
 });
 
+/**
+ * GET
+ */
 $app->get('/{code}', function (Application $app, $code) use ($items) {
 	if (!isset($items[$code])) {
 		$app->abort(404, "{$code} was not found.");
 	}
 	return json_encode($items[$code]);
 });
+
+/**
+ * POST
+ */
+$app->post('/', function (Application $app, Request $request) {
+	$name = $request->get('name');
+
+	// todo do stuff with data
+	$item = [
+		'3' => [
+			'name' => 'item3',
+		],
+	];
+
+	return new Response(json_encode($item), Response::HTTP_CREATED);
+});
+
+
+
 
 $app->run();
